cfad2a06aca076b774c900424ba40c33a946aa7c
amine.chikouche@gmail.com <amine.chikouche@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-01-19 15:44:07 +0000

    tag pour additionneur!

---
+++ tag/src/m1/moduleProjet/traitement/Multiplicateur.java
@@ -0,0 +1,18 @@
+package m1.moduleProjet.traitement;
+
+public class Multiplicateur {
+
+	private float operande1, operande2;
+
+	public Multiplicateur(float op1, float op2) {
+		// TODO Auto-generated constructor stub
+		this.operande1 = op1;
+		this.operande2 = op2;
+	}
+
+	public float multiplication() {
+
+		return (operande1*operande2);
+	}
+
+}
