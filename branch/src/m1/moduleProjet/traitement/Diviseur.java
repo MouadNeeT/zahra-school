42d9c895c38b4705db72c18a9161033380597ab4
amine.chikouche@gmail.com <amine.chikouche@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-01-26 17:35:07 +0000

    initial version diviseur complet

---
+++ branch/src/m1/moduleProjet/traitement/Diviseur.java
@@ -0,0 +1,18 @@
+package m1.moduleProjet.traitement;
+
+public class Diviseur {
+
+	private float operande1, operande2;
+
+	public Diviseur(float op1, float op2) {
+		// TODO Auto-generated constructor stub
+		this.operande1 = op1;
+		this.operande2 = op2;
+	}
+
+	public float division() {
+
+		return (operande1/operande2);
+	}
+
+}
