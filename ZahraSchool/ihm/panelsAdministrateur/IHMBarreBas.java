ea87f78bdb547cded9cedbf1ae741d9f9a5304eb
stephanie.gautier1 <stephanie.gautier1@gmail.com>
2012-03-25 15:56:46 +0000

    commit des panels

--- ZahraSchool/ihm/panelsAdministrateur/IHMBarreBas.java
+++ ZahraSchool/ihm/panelsAdministrateur/IHMBarreBas.java
@@ -1,5 +1,4 @@
 package panelsAdministrateur;
-
 import java.awt.Color;
 import java.awt.Font;
 
@@ -26,75 +25,94 @@ public class IHMBarreBas extends JPanel{
     
     
     public void initComponents(){
-    	
-    	setBackground(new Color(102, 102, 255));
-        setPreferredSize(getPreferredSize());
 
-        jButton1.setBackground(new Color(102, 102, 255));
-        jButton1.setFont(new Font("Tahoma", 1, 11));
-        jButton1.setForeground(new Color(0, 0, 153));
-        jButton1.setText("Informations sur le site");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jPanel1 = new javax.swing.JPanel();
+        boutonReglementInterieur = new javax.swing.JButton();
+        boutonAide = new javax.swing.JButton();
+        boutonInformationsSurLeSite = new javax.swing.JButton();
+
+        jPanel1.setBackground(new java.awt.Color(204, 204, 255));
+        jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255)));
+
+        boutonReglementInterieur.setBackground(new java.awt.Color(204, 204, 255));
+        boutonReglementInterieur.setFont(new java.awt.Font("Tahoma", 2, 12));
+        boutonReglementInterieur.setForeground(new java.awt.Color(0, 0, 153));
+        boutonReglementInterieur.setText("Règlement intérieur");
+        boutonReglementInterieur.setBorder(null);
+
+        boutonAide.setBackground(new java.awt.Color(204, 204, 255));
+        boutonAide.setFont(new java.awt.Font("Tahoma", 2, 12));
+        boutonAide.setForeground(new java.awt.Color(0, 0, 153));
+        boutonAide.setText("Aide ?");
+        boutonAide.setBorder(null);
+        boutonAide.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                boutonAideActionPerformed(evt);
             }
         });
 
-        jButton2.setBackground(new Color(102, 102, 255));
-        jButton2.setFont(new Font("Tahoma", 1, 11));
-        jButton2.setForeground(new Color(0, 0, 153));
-        jButton2.setText("Aide ?");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        boutonInformationsSurLeSite.setBackground(new java.awt.Color(204, 204, 255));
+        boutonInformationsSurLeSite.setFont(new java.awt.Font("Tahoma", 2, 12));
+        boutonInformationsSurLeSite.setForeground(new java.awt.Color(0, 0, 153));
+        boutonInformationsSurLeSite.setText("Informations sur le site");
+        boutonInformationsSurLeSite.setBorder(null);
+        boutonInformationsSurLeSite.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                boutonInformationsSurLeSiteActionPerformed(evt);
             }
         });
 
-        jButton3.setBackground(new Color(102, 102, 255));
-        jButton3.setFont(new Font("Tahoma", 1, 11));
-        jButton3.setForeground(new Color(0, 0, 153));
-        jButton3.setText("Règlement intérieur");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
-            }
-        });
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(82, 82, 82)
+                .addComponent(boutonInformationsSurLeSite, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(104, 104, 104)
+                .addComponent(boutonAide, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 473, Short.MAX_VALUE)
+                .addComponent(boutonReglementInterieur, javax.swing.GroupLayout.PREFERRED_SIZE, 122, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(46, 46, 46))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(14, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(boutonReglementInterieur)
+                    .addComponent(boutonAide)
+                    .addComponent(boutonInformationsSurLeSite))
+                .addGap(21, 21, 21))
+        );
 
-        GroupLayout layout = new GroupLayout(this);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1)
-                .addGap(18, 18, 18)
-                .addComponent(jButton2)
-                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 554, Short.MAX_VALUE)
-                .addComponent(jButton3)
-                .addContainerGap())
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2)
-                    .addComponent(jButton3))
-                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
     }// </editor-fold>
-    
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                         
-        // TODO add your handling code here:
-    }                                        
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+    private void boutonAideActionPerformed(java.awt.event.ActionEvent evt) {                                           
         // TODO add your handling code here:
-    }                                        
+    }                                          
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+    private void boutonInformationsSurLeSiteActionPerformed(java.awt.event.ActionEvent evt) {                                                            
         // TODO add your handling code here:
-    }    
+    }                                                           
+
+
+    // Variables declaration - do not modify
+    private javax.swing.JButton boutonAide;
+    private javax.swing.JButton boutonInformationsSurLeSite;
+    private javax.swing.JButton boutonReglementInterieur;
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration
 
 }
+
