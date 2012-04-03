6806b231e10978c836929ae0533eff0e43fbef66
waelrawas9@gmail.com <waelrawas9@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-03 18:55:33 +0000

    test

--- trunk/ZahraSchool/ihm/frames/FFondFenetreProfesseur.java
+++ trunk/ZahraSchool/ihm/frames/FFondFenetreProfesseur.java
@@ -1,7 +1,5 @@
 package frames;
 
-import panels.*;
-
 
 
 
@@ -24,32 +22,34 @@ public class FFondFenetreProfesseur extends JFrame{
 	IHMMenuGaucheProfesseur menuGauche = new IHMMenuGaucheProfesseur();
 	IHMBarreBas barreBas = new IHMBarreBas();
 	IHMMenuPrincipalAdministrateur principalMenu = new IHMMenuPrincipalAdministrateur();
-        FaireLapelle1 appel1 = new FaireLapelle1();
-	
-	public FFondFenetreProfesseur(){
-		
-		
-		JPanel panel1 = new JPanel(new BorderLayout());
-		
-		/**
-		 * Il faut ajouter le panel que vous voulez ici.
-		 * Rajouter donc le panel : ne pas oublier de mettre le
-		 * borderLayout � WEST
-		 */
-                //appel1.setLayout( new BorderLayout());
+        JPanel panel1 = new JPanel(new BorderLayout());
+        JPanel t=null;
+          
+	public FFondFenetreProfesseur(JPanel j)){
 
 		panel1.add(menuGauche, BorderLayout.WEST);
-		panel1.add(barreHaut, BorderLayout.NORTH);
-		panel1.add(appel1, BorderLayout.CENTER);
-		panel1.add(barreBas, BorderLayout.SOUTH);
+	        panel1.add(barreHaut, BorderLayout.NORTH);
+                panel1.add(barreBas, BorderLayout.SOUTH);
+               
+
+	}
+        
+        public JPanel getPanel(){
+            return t;
+        }
+        
+        public void setPanel(JPanel j){
+            
+               panel1.remove(getPanel());
+               t=j;
+		panel1.add(getPanel(), BorderLayout.CENTER);
 		add(panel1);
 		panel1.setPreferredSize(new Dimension(800, 500));
 		setSize(new Dimension(800, 600));
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-
-	}
+        }
 	
 
 }
