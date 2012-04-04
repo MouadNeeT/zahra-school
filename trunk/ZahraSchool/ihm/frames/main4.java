548d08991bd8be5c744116c153c1ac0cd60392f6
waelrawas9@gmail.com <waelrawas9@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-04 16:19:00 +0000

    Changer Panel

--- trunk/ZahraSchool/ihm/frames/main4.java
+++ trunk/ZahraSchool/ihm/frames/main4.java
@@ -7,9 +7,8 @@ public class main4 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		FFondFenetreProfesseur fenetreFondProfesseur =  new FFondFenetreProfesseur();
-        panels.FaireLapelle1 f1 = new panels.FaireLapelle1(fenetreFondProfesseur);
-        fenetreFondProfesseur.setPanel(f1);
+		FConnexionZahraSchool f = new FConnexionZahraSchool();
+        
 	}
 
 }
