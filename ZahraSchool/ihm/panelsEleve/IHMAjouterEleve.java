abac6256d868438a3babc5805f51bc13cea344a9
waelrawas9 <waelrawas9@gmail.com>
2012-04-04 18:58:51 +0000

    Changer Panel

--- ZahraSchool/ihm/panelsEleve/IHMAjouterEleve.java
+++ ZahraSchool/ihm/panelsEleve/IHMAjouterEleve.java
@@ -341,7 +341,8 @@ public class IHMAjouterEleve extends javax.swing.JPanel {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+      panelsEleve.IHMChoixEleve2 F1 = new panelsEleve.IHMChoixEleve2(f);
+      f.setPanel(F1);
     }//GEN-LAST:event_jButton2ActionPerformed
 
 
