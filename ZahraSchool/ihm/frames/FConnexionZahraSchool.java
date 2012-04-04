1efa19c878e9e0d72880b714cda59219e91d5909
stephanie.gautier1 <stephanie.gautier1@gmail.com>
2012-04-04 12:22:16 +0000

    changement connexion zahraschool

---
+++ ZahraSchool/ihm/frames/FConnexionZahraSchool.java
@@ -0,0 +1,32 @@
+package frames;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JWindow;
+
+import panelsAdministrateur.IHMConnexion;
+
+public class FConnexionZahraSchool extends JWindow{
+	
+	frames.FFondFenetreAdministrateur frame;
+	JPanel panel = new JPanel();
+    JPanel t=null;
+    IHMConnexion connexion = new IHMConnexion(frame);
+    
+    public FConnexionZahraSchool(){
+    	panel.add(connexion);
+    	getContentPane().add(panel);	
+    	setSize(new Dimension(500, 400));  	
+		setLocationRelativeTo(null);
+		setVisible(true);       
+    }
+    
+    public static void main(String[] args) {
+    	FConnexionZahraSchool f = new FConnexionZahraSchool();
+
+	}
+}
