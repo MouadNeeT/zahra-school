f12863a525fdd0f1909dfdd21ce7dcc2afb20d05
stephanie.gautier1@gmail.com <stephanie.gautier1@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-06 19:32:10 +0000

    création d'un horloge à mettre a gauche si vous voulez

---
+++ trunk/ZahraSchool/ihm/panels/Horloge.java
@@ -0,0 +1,37 @@
+package panels;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.text.*;
+import java.util.Date;
+import javax.swing.*;
+
+public class Horloge  extends JPanel{   
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public Horloge(){
+		
+		DateFormat date = DateFormat.getDateInstance(DateFormat.FULL);
+	    SimpleDateFormat heure = new SimpleDateFormat("HH 'h' mm 'minutes'");
+	    JPanel panel = new JPanel(new BorderLayout());
+	    JPanel panel2 = new JPanel(new BorderLayout());
+	    JLabel heureAffichage = new JLabel("    " +heure.format(new Date()));
+	    JLabel dateAffichage = new JLabel("    " +date.format(new Date()));
+	    JLabel image = new JLabel();
+	    //image.setIcon(new ImageIcon("C://Users//Evi and Max//Desktop//horloge.jpg"));
+	    panel.add(heureAffichage, BorderLayout.CENTER);
+	    panel.add(dateAffichage, BorderLayout.NORTH);
+	    panel.add(image, BorderLayout.EAST);
+	    panel.setBackground(Color.white);
+	    panel2.add(panel, BorderLayout.CENTER);
+	    add(panel2, BorderLayout.CENTER);
+	    panel2.setSize(150, 50);
+	    setSize(150, 90);
+		
+	}
+  public static void main(String[] args) { 
+	  Horloge h = new Horloge();
+   }
+}
