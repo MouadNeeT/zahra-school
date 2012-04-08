ffb3b44a8c0fe23593c83cbbc086baa4fe31e3ca
waelrawas9@gmail.com <waelrawas9@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-08 12:41:34 +0000

    commit pour Matiere

--- trunk/ZahraSchool/src/pattern/beans/Matiere.java
+++ trunk/ZahraSchool/src/pattern/beans/Matiere.java
@@ -1,5 +1,35 @@
 package pattern.beans;
 
 public class Matiere {
-
+    
+    private String nom;
+    private String type;
+    
+    public Matiere()
+    {}
+    
+    public Matiere(String nom, String type)
+    {
+        this.nom=nom;
+        this.type= type;
+    }
+    
+    public void setNom(String nom)
+    {
+        this.nom=nom;
+    }
+    public String getNom()
+    {
+        return this.nom;
+    }
+    
+     public void settype(String type)
+    {
+        this.type=type;
+    }
+    public String gettype()
+    {
+        return this.type;
+    }
+    
 }
