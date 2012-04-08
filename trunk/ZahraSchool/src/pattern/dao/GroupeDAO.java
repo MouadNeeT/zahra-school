4f5712c554fe65c105f83156b53888985d37a7b8
waelrawas9@gmail.com <waelrawas9@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-08 14:33:11 +0000

    commit DAO

--- trunk/ZahraSchool/src/pattern/dao/GroupeDAO.java
+++ trunk/ZahraSchool/src/pattern/dao/GroupeDAO.java
@@ -1,5 +1,59 @@
 package pattern.dao;
+import pattern.beans.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.util.Date;
 
-public class GroupeDAO {
+public class GroupeDAO extends DAO<Groupe> {
+
+        public GroupeDAO(Connection connexion) {
+                super(connexion);
+        }
+
+    @Override
+        public boolean create(Groupe obj) {
+                return false;
+        }
+
+    @Override
+        public boolean delete(Groupe obj) {
+                return false;
+        }
+
+        public Groupe find(String nom) {
+                
+                Groupe groupe = new Groupe();                
+                
+                try {
+                        ResultSet result = this.connect.createStatement(
+                                                               ResultSet.TYPE_SCROLL_INSENSITIVE, 
+                                                               ResultSet.CONCUR_READ_ONLY
+                                                        ).executeQuery(
+                                                               "SELECT * FROM groupe WHERE nom = " + nom
+                                                              
+                                                          );
+                        if(result.first())
+                        	
+                                groupe = new Groupe(nom, result.getString("niveau"), 
+                                		result.getDate("dateDeCreation"), 
+                                		result.getFloat("tarif"));
+                /*          public Groupe(String nom, String niveau ,Date dateDeCreation, float tarif)*/
+
+                } catch (SQLException e) {
+                        e.printStackTrace();
+                }
+                return groupe;
+        }
+
+    @Override
+        public boolean update(Groupe obj) {
+                return false;
+        }
+
+    @Override
+    public Groupe find(String nom, String prenom) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
