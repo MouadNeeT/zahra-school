af277ae6c23e1687c6cbc161209c7f6097b57908
waelrawas9@gmail.com <waelrawas9@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-08 12:38:14 +0000

    commit de bean groupe

--- trunk/ZahraSchool/src/pattern/beans/Groupe.java
+++ trunk/ZahraSchool/src/pattern/beans/Groupe.java
@@ -1,5 +1,65 @@
 package pattern.beans;
 
+import java.util.Date;
+
 public class Groupe {
 
+    private String nom;
+    
+    private String niveau;
+    
+    private Date dateDeCreation;
+    
+    private float tarif;
+
+    public Groupe(){}
+    
+    public Groupe(String nom, String niveau ,Date dateDeCreation, float tarif)
+    {
+        this.nom=nom;
+        this.niveau=niveau;
+        this.dateDeCreation=dateDeCreation;
+        this.tarif=tarif;
+    }
+    
+    public void setNom(String Nom){
+        this.nom= Nom;
+    }
+    
+    public String getNom()
+    {
+        return this.nom;
+    }
+    
+    public void setNiveau(String Niveau)
+    {
+        this.niveau=Niveau;
+    }
+    
+    public String getNiveau()
+    {
+        return this.niveau;
+    }
+    
+    public void setDate(Date dateDeCreation)
+    {
+        this.dateDeCreation=dateDeCreation;
+    }
+    
+    public Date getDateDeCreation()
+    {
+        return this.dateDeCreation;
+    }
+    
+    public void setTarif(float tarif)
+    {
+        this.tarif=tarif;
+    }
+    
+    public float returnTarif()
+    {
+        return this.tarif;
+    }
+    
+    
 }
