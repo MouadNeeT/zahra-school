c0c2c78e7df478dae5fedcd4a6b53a7740611878
stephanie.gautier1@gmail.com <stephanie.gautier1@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-08 10:48:50 +0000

    commit des pattern

--- trunk/ZahraSchool/pattern/beans/Professeur.java
+++ trunk/ZahraSchool/pattern/beans/Professeur.java
@@ -1,5 +1,17 @@
 package beans;
 
 public class Professeur {
+	
+	private String identifiant;
+	private String motDePasse;
+	private String nom;
+	private String prenom;
+	private String adresse;
+	private int numeroTelephone;
+	private String niveauEtudes;
+	private Date dateDeNaissance;
+	private Date dateEmbauche;
+	
+	
 
 }
