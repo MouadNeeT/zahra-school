e97b033a73dba09c975bcdabd0b548f2f040c963
waelrawas9 <waelrawas9@gmail.com>
2012-04-08 12:46:35 +0000

    commit pour absence

--- ZahraSchool/src/pattern/beans/Absence.java
+++ ZahraSchool/src/pattern/beans/Absence.java
@@ -1,5 +1,38 @@
 package pattern.beans;
 
+import java.util.Date;
+
 public class Absence {
+    
+    private String motif;
+    private Date date;
+    
+    public Absence(){}
+    
+    public Absence(String motif, Date date)
+    {
+        this.motif=motif;
+        this.date=date;
+    }
+    
+     public void setMotif(String Motif){
+        this.motif= Motif;
+    }
+    
+    public String getmotif()
+    {
+        return this.motif;
+    }
+    
+      public void setDate(Date date)
+    {
+        this.date=date;
+    }
+    
+    public Date getDate()
+    {
+        return this.date;
+    }
+    
 
 }
