e0eb51e4ed02f66efa6f4ae907839c75bba37013
alexandre.sacareau@gmail.com <alexandre.sacareau@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-10 15:02:59 +0000

    mise a jour presentation

--- trunk/ZahraSchool/ihm/panelsGroupe/IHMRechercherGroupe.java
+++ trunk/ZahraSchool/ihm/panelsGroupe/IHMRechercherGroupe.java
@@ -13,7 +13,7 @@ package panelsGroupe;
 
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
-import panelsAdministrateur.IHMBarreVisionGestionEleve;
+import panelsAdministrateur.IHMBarreVisionGestion;
 
 /**
  *
@@ -170,7 +170,7 @@ public class IHMRechercherGroupe extends javax.swing.JPanel {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         panelsGroupe.IHMChoixGroupe CG = new  panelsGroupe.IHMChoixGroupe(f);
-        IHMBarreVisionGestionEleve barreVision = new IHMBarreVisionGestionEleve(f,"   Gestion des groupes");
+        IHMBarreVisionGestion barreVision = new IHMBarreVisionGestion(f,"   Gestion des groupes");
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(CG, BorderLayout.CENTER);
         panel.add(barreVision, BorderLayout.NORTH);
