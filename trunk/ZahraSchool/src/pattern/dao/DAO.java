60b9a70783d9833663d612da65c752f0c701dadd
waelrawas9@gmail.com <waelrawas9@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-10 00:09:09 +0000

    enlever les commentaire

--- trunk/ZahraSchool/src/pattern/dao/DAO.java
+++ trunk/ZahraSchool/src/pattern/dao/DAO.java
@@ -10,36 +10,19 @@ public abstract class DAO<T> {
         
         protected Connection connect = null;
         
-        /**
-         * Constructeur
-         * @param conn
-         */
+      
         public DAO(Connection conn){
                 this.connect = conn;
         }
         
-        /**
-         * Méthode de création
-         * @param obj
-         * @return
-         */
+      
         public abstract boolean create(T obj);
-        /**
-         * Méthode pour effacer
-         * @param obj
-         * @return
-         */
+       
         public abstract boolean delete(T obj);
-        /**
-         * Méthode de mise à jour
-         * @param obj
-         * @return
-         */
+        
         public abstract boolean update(T obj);
-        /**
-         * Méthode de recherche des informations
-         * @param id
-         * @return
-         */
+        
         public abstract T find(String nom, String prenom);
+        
+    
 }
