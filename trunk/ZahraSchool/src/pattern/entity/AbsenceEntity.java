ae87d63b257edacdb094f9d53b311bd400467478
stephanie.gautier1@gmail.com <stephanie.gautier1@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-11 11:01:44 +0000

    absence entity terminé

--- trunk/ZahraSchool/src/pattern/entity/AbsenceEntity.java
+++ trunk/ZahraSchool/src/pattern/entity/AbsenceEntity.java
@@ -1,9 +1,73 @@
 /**
- * author GAUTIER Stephanie
+ * author GAUTIER Stéphanie
  */
 
 package pattern.entity;
 
-public class AbsenceEntity {
+import java.util.Date;
+
+import pattern.beans.Absence;
+import pattern.beans.Eleve;
+
+public class AbsenceEntity extends Entity {
+
+	/**
+     * le motif de l'absence de l'élève.
+     * @see Absence#setMotif(String)
+     * @see Absence#getMotif()
+     */
+    private String motif;
+    private Date date;
+    private Eleve eleve;
+    
+    public AbsenceEntity(){}
+    
+    public AbsenceEntity(Eleve eleve, String motif, Date date)
+    {
+    	this.eleve=eleve;
+        this.motif=motif;
+        this.date=date;
+    }
+    
+     /**
+	 * @return the eleve
+	 */
+	public Eleve getEleve() {
+		return eleve;
+	}
+
+	/**
+	 * @param eleve the eleve to set
+	 */
+	public void setEleve(Eleve eleve) {
+		this.eleve = eleve;
+	}
+
+	/**
+	 * @return the motif
+	 */
+	public String getMotif() {
+		return motif;
+	}
+
+	public void setMotif(String Motif){
+        this.motif= Motif;
+    }
+    
+    public String getmotif()
+    {
+        return this.motif;
+    }
+    
+      public void setDate(Date date)
+    {
+        this.date=date;
+    }
+    
+    public Date getDate()
+    {
+        return this.date;
+    }
+    
 
 }
