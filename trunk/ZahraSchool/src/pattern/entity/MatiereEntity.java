c0a857bee378f56c9533dfd36a032ea01fa621ec
stephanie.gautier1@gmail.com <stephanie.gautier1@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-11 11:03:55 +0000

    matiere entity terminé

--- trunk/ZahraSchool/src/pattern/entity/MatiereEntity.java
+++ trunk/ZahraSchool/src/pattern/entity/MatiereEntity.java
@@ -1,9 +1,110 @@
 /**
- * author GAUTIER Stephanie
+ * author GAUTIER Stéphanie
  */
 
 package pattern.entity;
 
-public class MatiereEntity {
+import java.util.ArrayList;
 
+import pattern.beans.EmploiDuTemps;
+import pattern.beans.Groupe;
+import pattern.beans.Professeur;
+
+public class MatiereEntity extends Entity{
+	private String nom;
+    private String type;
+    private ArrayList<EmploiDuTemps> listeEmploiDuTemps;
+    private ArrayList<Professeur> listeProfesseurs;
+    private ArrayList<Groupe> listeGroupes;
+    
+    public MatiereEntity(){}
+    
+    public MatiereEntity(String nom, String type, ArrayList<Professeur> listeProfesseurs,
+    		ArrayList<Groupe> listeGroupes, ArrayList<EmploiDuTemps> listeEmploiDuTemps)
+    {
+        this.nom=nom;
+        this.type= type;
+        this.listeEmploiDuTemps = listeEmploiDuTemps;
+        this.listeProfesseurs = listeProfesseurs;
+        this.listeGroupes = listeGroupes;
+    }
+    
+    public void setNom(String nom)
+    {
+        this.nom=nom;
+    }
+    public String getNom()
+    {
+        return this.nom;
+    }
+    
+     public void setType(String type)
+    {
+        this.type=type;
+    }
+    public String getType()
+    {
+        return this.type;
+    }
+
+	/**
+	 * @param e
+	 * @return
+	 * @see java.util.ArrayList#add(java.lang.Object)
+	 */
+	public boolean add(EmploiDuTemps e) {
+		return listeEmploiDuTemps.add(e);
+	}
+
+	/**
+	 * @param o
+	 * @return
+	 * @see java.util.ArrayList#remove(java.lang.Object)
+	 */
+	public boolean remove(Object o) {
+		return listeEmploiDuTemps.remove(o);
+	}
+
+	/**
+	 * @return the listeEmploiDuTemps
+	 */
+	public ArrayList<EmploiDuTemps> getListeEmploiDuTemps() {
+		return listeEmploiDuTemps;
+	}
+
+	/**
+	 * @param listeEmploiDuTemps the listeEmploiDuTemps to set
+	 */
+	public void setListeEmploiDuTemps(ArrayList<EmploiDuTemps> listeEmploiDuTemps) {
+		this.listeEmploiDuTemps = listeEmploiDuTemps;
+	}
+
+	/**
+	 * @return the listeProfesseurs
+	 */
+	public ArrayList<Professeur> getListeProfesseurs() {
+		return listeProfesseurs;
+	}
+
+	/**
+	 * @param listeProfesseurs the listeProfesseurs to set
+	 */
+	public void setListeProfesseurs(ArrayList<Professeur> listeProfesseurs) {
+		this.listeProfesseurs = listeProfesseurs;
+	}
+
+	/**
+	 * @return the listeGroupes
+	 */
+	public ArrayList<Groupe> getListeGroupes() {
+		return listeGroupes;
+	}
+
+	/**
+	 * @param listeGroupes the listeGroupes to set
+	 */
+	public void setListeGroupes(ArrayList<Groupe> listeGroupes) {
+		this.listeGroupes = listeGroupes;
+	}
+    
 }
