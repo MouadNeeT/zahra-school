6bf952feb3910f6ee80d6b7693e0db75beea32be
stephanie.gautier1@gmail.com <stephanie.gautier1@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-11 11:04:06 +0000

    paiement entity terminé

--- trunk/ZahraSchool/src/pattern/entity/PaiementEntity.java
+++ trunk/ZahraSchool/src/pattern/entity/PaiementEntity.java
@@ -1,9 +1,86 @@
 /**
- * author GAUTIER Stephanie
+ * author GAUTIER Stéphanie
  */
 
 package pattern.entity;
 
+import java.util.Date;
+
+import pattern.beans.Eleve;
+
 public class PaiementEntity {
+	private int montant;
+	private String mois;
+	private Date dateDePaiementPrevus;
+	private Date dateEncaissement;
+	private Eleve eleve;
+	
+	public PaiementEntity() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public PaiementEntity(int montant, String mois, Date dateDePaiementPrevus, Date dateEncaissement, Eleve eleve) {
+		this.montant = montant;
+		this.mois = mois;
+		this.dateDePaiementPrevus = dateDePaiementPrevus;
+		this.dateEncaissement = dateEncaissement;
+		this.eleve = eleve;
+	}
+	
+	
+	public int getMontant() {
+		return montant;
+	}
+
+	public void setMontant(int montant) {
+		this.montant = montant;
+	}
+
+	public String getMois() {
+		return mois;
+	}
+
+	public void setMois(String mois) {
+		this.mois = mois;
+	}
+
+	public Date getDateDePaiementPrévus() {
+		return dateDePaiementPrevus;
+	}
+
+	public void setDateDePaiementPrevus(Date dateDePaiementPrevus) {
+		this.dateDePaiementPrevus = dateDePaiementPrevus;
+	}
+
+	/**
+	 * @return the eleve
+	 */
+	public Eleve getEleve() {
+		return eleve;
+	}
+
+	/**
+	 * @param eleve the eleve to set
+	 */
+	public void setEleve(Eleve eleve) {
+		this.eleve = eleve;
+	}
+
+	/**
+	 * @return the dateDePaiementPrevus
+	 */
+	public Date getDateDePaiementPrevus() {
+		return dateDePaiementPrevus;
+	}
+
+	public Date getDateEncaissement() {
+		return dateEncaissement;
+	}
+
+	public void setDateEncaissement(Date dateEncaissement) {
+		this.dateEncaissement = dateEncaissement;
+	}
+
+	
 
 }
