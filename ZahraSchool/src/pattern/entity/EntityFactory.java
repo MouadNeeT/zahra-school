57c75f1ce7cd7c39dce720442613e86796ecb026
stephanie.gautier1 <stephanie.gautier1@gmail.com>
2012-04-11 11:02:45 +0000

    factory entity terminé

--- ZahraSchool/src/pattern/entity/EntityFactory.java
+++ ZahraSchool/src/pattern/entity/EntityFactory.java
@@ -1,9 +1,85 @@
 /**
- * author GAUTIER Stephanie
+ * author GAUTIER Stéphanie
  */
 
 package pattern.entity;
 
 public class EntityFactory {
+	/**
+     * Retourne un objet Absence interagissant avec la BDD
+     * @return
+     */
+    public static AbsenceEntity getAbsenceEntity(){
+            return new AbsenceEntity();
+    }
+    
+    /**
+     * Retourne un objet administrateur interagissant avec la BDD
+     * @return AdministrateurEntity()
+     */
+    public static AdministrateurEntity getAdministrateurEntity(){
+    	return new AdministrateurEntity();
+    }
+    /**
+     * Retourne un objet Eleve interagissant avec la BDD
+     * @return
+     */
+    public static EleveEntity getEleveEntity(){
+            return new EleveEntity();
+    }
+    /**
+     * Retourne un objet EmploiDuTemps interagissant avec la BDD
+     * @return
+     */
+    public static EmploiDuTempsEntity getEmploiDuTempsEntity(){
+            return new EmploiDuTempsEntity();
+    }
+    /**
+     * Retourne un objet Groupe interagissant avec la BDD
+     * @return
+     */
+    public static GroupeEntity getGroupeEntity(){
+            return new GroupeEntity();
+    } 
+    
+    /**
+     * Retourne un objet Heure interagissant avec la BDD
+     * @return
+     */
+    public static HeureEntity getHeureEntity(){
+    	return new HeureEntity();
+    }
+    
+    /**
+     * Retourne un objet Jour interagissant avec la BDD
+     * @return
+     */
+    public static JourEntity getJourEntity(){
+            return new JourEntity();
+    }
+    
+    /**
+     * Retourne un objet Matiere interagissant avec la BDD
+     * @return
+     */
+    public static MatiereEntity getMatiereDAO(){
+            return new MatiereEntity();
+    }
+    
+    /**
+     * Retourne un objet Paiement interagissant avec la BDD
+     * @return
+     */
+    public static PaiementEntity getPaiementEntity(){
+            return new PaiementEntity();
+    }
+      
+    /**
+     * Retourne un objet Professeur interagissant avec la BDD
+     * @return
+     */
+    public static ProfesseurEntity getProfesseurEntity(){
+        return new ProfesseurEntity();
+    }
 
 }
