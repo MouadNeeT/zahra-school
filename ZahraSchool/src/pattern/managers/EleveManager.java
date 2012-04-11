27b7fdfd865e65881aafe7b82ea7bdc0f7026c68
stephanie.gautier1 <stephanie.gautier1@gmail.com>
2012-04-11 15:59:19 +0000

    exemple de manager avec des fautes à corriger

--- ZahraSchool/src/pattern/managers/EleveManager.java
+++ ZahraSchool/src/pattern/managers/EleveManager.java
@@ -1,5 +1,51 @@
 package pattern.managers;
 
-public class EleveManager {
+import pattern.dao.DAO;
+import pattern.entity.EleveEntity;
 
-}
+public class EleveManager {
+	private EleveManager(){
+		
+	}
+	 private static class ManagerEleveHolder {
+	        private final static EleveManager instance = new EleveManager();
+	    }
+
+	    /**
+	     * Méthode permettant d'obtenir l'instance unique.
+	     * @return L'instance du singleton.
+	     */
+	    public static EleveManager getInstance() {
+	        return ManagerEleveHolder.instance;
+	    }
+
+	@Override
+	public  void  createAndStore(Eleve eleve) {
+		//
+		EleveEntity eleveEntity = EntityFactory.getEleveEntity();
+		eleveEntity.setNom(eleve.getNom());
+		eleveEntity.setPrenom(eleve.getPrenom());
+		
+		
+		
+		// TODO Auto-generated method stub
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = null;
+		try {
+		tx = session.beginTransaction();
+		// do some work
+		DAO<EleveEntity> eleveDAO = DAOFactory.getEleveDAO();
+		
+		
+		eleveDAO.create(eleveEntity);
+		
+		
+		tx.commit();
+		}
+		catch (RuntimeException e) {
+		if (tx != null) tx.rollback();
+		throw e; // or display error message
+		}finally {
+			session.close();
+		}
+	}
