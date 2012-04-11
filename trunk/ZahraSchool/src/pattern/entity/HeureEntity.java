1785383b4543c687d091816dea29d94e487b8946
stephanie.gautier1@gmail.com <stephanie.gautier1@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-11 11:03:32 +0000

    heure entity terminé

--- trunk/ZahraSchool/src/pattern/entity/HeureEntity.java
+++ trunk/ZahraSchool/src/pattern/entity/HeureEntity.java
@@ -1,9 +1,33 @@
 /**
- * author GAUTIER Stephanie
+ * author GAUTIER Stéphanie
  */
 
 package pattern.entity;
 
+import pattern.beans.Jour;
+
 public class HeureEntity {
+private Jour jour;
+	
+	public HeureEntity(){}
+	
+	public HeureEntity(Jour jour){
+		this.jour = jour;
+	}
+
+	/**
+	 * @return the jour
+	 */
+	public Jour getJour() {
+		return jour;
+	}
+
+	/**
+	 * @param jour the jour to set
+	 */
+	public void setJour(Jour jour) {
+		this.jour = jour;
+	}
+	
 
 }
