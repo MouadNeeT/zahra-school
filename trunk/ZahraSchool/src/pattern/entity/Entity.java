cb02dd4d9cbca4a56249d956d3cfebd9753468ea
alexandre.sacareau@gmail.com <alexandre.sacareau@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-25 20:50:23 +0000

    Entity

--- trunk/ZahraSchool/src/pattern/entity/Entity.java
+++ trunk/ZahraSchool/src/pattern/entity/Entity.java
@@ -1,5 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package pattern.entity;
 
+/**
+ *
+ * @author alexandre
+ */
 public class Entity {
 
 }
