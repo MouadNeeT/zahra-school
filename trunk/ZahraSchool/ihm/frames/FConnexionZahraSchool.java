1067a651238f134c3e6c8f778ed6a0b16858860f
alexandre.sacareau@gmail.com <alexandre.sacareau@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-04-25 21:37:46 +0000

    Reorganisation

--- trunk/ZahraSchool/ihm/frames/FConnexionZahraSchool.java
+++ trunk/ZahraSchool/ihm/frames/FConnexionZahraSchool.java
@@ -10,11 +10,11 @@ import javax.swing.JWindow;
 
 import panelsAdministrateur.IHMConnexion;
 
-public class FConnexionZahraSchool extends JFrame{
-	
-	frames.FFondFenetreAdministrateur frame;
-	frames.FFondFenetreProfesseur f2;
-	JPanel panel = new JPanel();
+public class FConnexionZahraSchool extends JFrame {
+    frames.FFondFenetreAdministrateur frame;
+    frames.FFondFenetreProfesseur f2;
+    JPanel panel = new JPanel();
+    
     JPanel t=null;
     IHMConnexion connexion = new IHMConnexion(frame, f2);
     
@@ -22,12 +22,12 @@ public class FConnexionZahraSchool extends JFrame{
     	panel.add(connexion);
     	getContentPane().add(panel);	
     	setSize(new Dimension(470, 450));  	
-		setLocationRelativeTo(null);
-		setVisible(true);       
+	setLocationRelativeTo(null);
+	setVisible(true);      
     }
-    
+
+    // Main
     public static void main(String[] args) {
     	FConnexionZahraSchool f = new FConnexionZahraSchool();
-
 	}
 }
