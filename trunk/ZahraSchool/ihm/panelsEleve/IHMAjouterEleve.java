02e99ed42d1ed20eaa7fae6e35ccbc2fcedfe04f
alexandre.sacareau@gmail.com <alexandre.sacareau@gmail.com@ae0d7774-ab87-5ba0-04f2-6fe163941370>
2012-05-04 17:30:29 +0000

    ajout createbean

--- trunk/ZahraSchool/ihm/panelsEleve/IHMAjouterEleve.java
+++ trunk/ZahraSchool/ihm/panelsEleve/IHMAjouterEleve.java
@@ -347,8 +347,9 @@ public class IHMAjouterEleve extends javax.swing.JPanel {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         JOptionPane jp = new JOptionPane();
         jp.showMessageDialog(null, "Ajouter l'eleve "+jTextField2.getText(), "Confirmation", JOptionPane.OK_CANCEL_OPTION);
-        panelsEleve.IHMAjouterEleve p = new  panelsEleve.IHMAjouterEleve(f);
-        f.setPanel(p);
+        panelsEleve.IHMAjouterEleve AE = new  panelsEleve.IHMAjouterEleve(f);
+        f.setPanel(AE);
+        //createBean(AE,1);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
